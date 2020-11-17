c8e27ee6f3c211dbba5c260229cde5a53b26b174
andrewtjong <andrewttj@yahoo.com>
2020-11-17 13:19:49 +0100

    mapStatetoProps, mapDispatchToProps

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from "react"
-
+import {connect} from "react-redux"
+import {increment, decrement} from "./redux"
+ 
 function App(props) {    
     return (
         <div>
-            
+          <h1>{props.count}</h1>
+            <button onClick={props.decrement}>-</button>
+            <button onClick={props.increment}>+</button>
         </div>
     )
 }
 
-export default App
+function mapStatetoProps(globalState) {
+  return {
+    count: globalState
+  }
+}
+
+const mapDispatchToProps = {
+  increment,
+  decrement
+}
+
+export default connect(mapStatetoProps, mapDispatchToProps) (App)
